87abf3b8dc277c69795968faae178c389faa84e5
Max Henning Junghans <MaxHenningJunghans@gmail.com>
2023-07-02 00:39:54 +0200

    Converted the program to Kotlin

--- src/Main.kt
+++ src/Main.kt
@@ -1,53 +1,54 @@
-import java.util.Scanner;
+import java.util.*
 
-public class Main {
+object Main {
+    private val scanner = Scanner(System.`in`)
+    @JvmStatic
+    fun main(args: Array<String>) {
+        runProgram()
+    }
 
-	private static final Scanner scanner = new Scanner(System.in);
+    private fun runProgram() {
+        var input: String
+        var finished = false
+        var timeCount = Time(0, 0)
+        while (!finished) {
+            input = scanner.nextLine()
+            when (input) {
+                "--stop" -> finished = true
+                "--reset" -> timeCount = Time(0, 0)
+                "--show" -> println(timeCount)
+                "--help" -> {
+                    println("This program has the following commands.")
+                    println("--help: Show all commands.")
+                    println("--stop: Stop the program.")
+                    println("--reset: Reset the counter to 0.")
+                    println("--show: Show the current value.")
+                    println("Inputs for this program have the following format.")
+                    println("<Hour of the first time>:<Minute of the first time> <Hour of the second time>:<Minute of the second time>")
+                    println("The times follow the 24-hour format. Here are some examples.")
+                    println("00:00 11:45")
+                    println("22:50 02:21")
+                    println("The program cannot count single durations longer than 24 hours.")
+                }
 
-	public static void main(String[] args) {
-		runProgram();
-	}
+                else -> processTimeInput(input, timeCount)
+            }
+        }
+    }
 
-	private static void runProgram() {
-		String input;
-		boolean finished = false;
-		Time timeCount = new Time(0, 0);
-
-		while (!finished) {
-			input = scanner.nextLine();
-			switch (input) {
-				case "--stop" -> finished = true;
-				case "--reset" -> timeCount = new Time(0, 0);
-				case "--show" -> System.out.println(timeCount);
-				case "--help" -> {
-					System.out.println("This program has the following commands.");
-					System.out.println("--help: Show all commands.");
-					System.out.println("--stop: Stop the program.");
-					System.out.println("--reset: Reset the counter to 0.");
-					System.out.println("--show: Show the current value.");
-					System.out.println("Inputs for this program have the following format.");
-					System.out.println("<Hour of the first time>:<Minute of the first time> <Hour of the second time>:<Minute of the second time>");
-					System.out.println("The times follow the 24-hour format. Here are some examples.");
-					System.out.println("00:00 11:45");
-					System.out.println("22:50 02:21");
-					System.out.println("The program cannot count single durations longer than 24 hours.");
-				}
-				default -> processTimeInput(input, timeCount);
-			}
-		}
-	}
-
-	private static void processTimeInput(String input, Time timeCount) {
-		try {
-			String[] times = input.split(" ");
-			String[] firstTimeString = times[0].split(":");
-			String[] secondTimeString = times[1].split(":");
-			Time firstTime = new Time(Integer.parseInt(firstTimeString[0]), Integer.parseInt(firstTimeString[1]));
-			Time secondTime = new Time(Integer.parseInt(secondTimeString[0]), Integer.parseInt(secondTimeString[1]));
-			timeCount.addToTime(Time.subtractTimes(firstTime, secondTime));
-			System.out.println(timeCount);
-		} catch (NumberFormatException | ArrayIndexOutOfBoundsException ignored) {
-			System.out.println("Wrong format. Please try again.");
-		}
-	}
+    private fun processTimeInput(input: String, timeCount: Time) {
+        try {
+            val times = input.split(" ".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+            val firstTimeString = times[0].split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+            val secondTimeString = times[1].split(":".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray()
+            val firstTime = Time(firstTimeString[0].toInt(), firstTimeString[1].toInt())
+            val secondTime = Time(secondTimeString[0].toInt(), secondTimeString[1].toInt())
+            timeCount.addToTime(Time.Companion.subtractTimes(firstTime, secondTime))
+            println(timeCount)
+        } catch (ignored: NumberFormatException) {
+            println("Wrong format. Please try again.")
+        } catch (ignored: ArrayIndexOutOfBoundsException) {
+            println("Wrong format. Please try again.")
+        }
+    }
 }
--- src/Time.kt
+++ src/Time.kt
@@ -1,37 +1,29 @@
-public class Time {
+class Time(private var hour: Int, private var minute: Int) {
+    fun addToTime(time: Time) {
+        hour += time.hour
+        minute += time.minute
+        if (minute >= 60) {
+            minute -= 60
+            hour++
+        }
+    }
 
-	private int hour;
-	private int minute;
+    override fun toString(): String {
+        return String.format("Stunden: %d, Minuten: %d", hour, minute)
+    }
 
-	public Time(int hour, int minute) {
-		this.hour = hour;
-		this.minute = minute;
-	}
-
-	public void addToTime(Time time) {
-		hour += time.hour;
-		minute += time.minute;
-		if (minute >= 60) {
-			minute -= 60;
-			hour++;
-		}
-	}
-
-	public static Time subtractTimes(Time firstTime, Time secondTime) {
-		int newHours = secondTime.hour - firstTime.hour;
-		int newMinutes = secondTime.minute - firstTime.minute;
-		if (newHours < 0) {
-			newHours = 24 + newHours;
-		}
-		if (newMinutes < 0) {
-			newMinutes = 60 + newMinutes;
-			newHours--;
-		}
-		return new Time(newHours, newMinutes);
-	}
-
-	@Override
-	public String toString() {
-		return String.format("Stunden: %d, Minuten: %d", hour, minute);
-	}
+    companion object {
+        fun subtractTimes(firstTime: Time, secondTime: Time): Time {
+            var newHours = secondTime.hour - firstTime.hour
+            var newMinutes = secondTime.minute - firstTime.minute
+            if (newHours < 0) {
+                newHours = 24 + newHours
+            }
+            if (newMinutes < 0) {
+                newMinutes = 60 + newMinutes
+                newHours--
+            }
+            return Time(newHours, newMinutes)
+        }
+    }
 }
